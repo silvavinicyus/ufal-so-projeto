ad69a7202ca4f29d6aae8dad14f895735c53c60d
Vinicyus <vinicyus346@gmail.com>
2021-04-28 17:58:58 -0300

    Refactoring loggings

--- questões/q3.py
+++ questões/q3.py
@@ -6,8 +6,9 @@ lock = Lock()
 saldoA = 500
 saldoB = 900
 
-def processo1():  
+def processo1():    
   lock.acquire()
+  logging.info("\nPerformando ações do Processo 1")
 
   global saldoA, saldoB
 
@@ -19,10 +20,15 @@ def processo1():
   x = x + 100
   saldoB = x
 
+  logging.info("Saldo após processo 1: ")
+  logging.info("Saldo A: %s", saldoA)
+  logging.info("Saldo b: %s", saldoB)
+  logging.info("Finalizando ações do Processo 1")
   lock.release()
 
-def processo2():
+def processo2():  
   lock.acquire()
+  logging.info("\nPerformando ações do Processo 2")
 
   global saldoA, saldoB
 
@@ -34,6 +40,10 @@ def processo2():
   y = y + 200
   saldoB = y
 
+  logging.info("Saldo após processo 2: ")
+  logging.info("Saldo A: %s", saldoA)
+  logging.info("Saldo b: %s", saldoB)
+  logging.info("Finalizando ações do Processo 2")
   lock.release()  
 
 if __name__ == '__main__':
@@ -44,9 +54,10 @@ if __name__ == '__main__':
   processo2 = Thread(target=processo2, args=())
 
   processo1.start()  
-  processo2.start()
   processo1.join()
+  processo2.start()
   processo2.join()
 
+  logging.info("\nSaldos finais: ")
   logging.info("Saldo A: %s", saldoA)
   logging.info("Saldo b: %s", saldoB)
